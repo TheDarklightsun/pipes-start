b3732971bcdcbada19c2a657fb44e3bdbc9e4037
TheDarklightsun <bublik.kirill2012@gmail.com>
2023-11-15 17:21:06 +0200

    finished

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {FormsModule} from "@angular/forms";
+import {ShortenPipe} from "./shorten.pipe";
+import {FilterPipe} from './filter.pipe';
+import { ReversePipe } from './reverse.pipe';
+import { SortPipe } from './sort.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ShortenPipe,
+    FilterPipe,
+    ReversePipe,
+    SortPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
